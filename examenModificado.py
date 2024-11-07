550f5506e87b47b8113672e8497655f4faa882ec
jose carlos florido cobo <xjflocob149@ieshnosmachado.org>
2024-11-07 11:19:57 +0100

    terminado

--- examenModificado.py
+++ examenModificado.py
@@ -6,13 +6,15 @@ musica=[]
 
 #Agregamos los datos del archivo playlist.txt al diccionario
 def agregarMusicaArchivo():
+    NumeroCanciones=0
     fichero=open("playlistPepe.txt","r")
     for linea in fichero:
+        NumeroCanciones=+1
         datos={}
-        cancion,cantante,genero=linea.split("-")
-        datos["Nombre: "]=cancion
-        datos["Cantante: "]=cantante
-        datos["Genero: "]=genero
+        cancion,cantante,genero=linea.strip().split(" - ")
+        datos["Nombre"]=cancion 
+        datos["Cantante"]=cantante
+        datos["Genero"]=genero
         musica.append(datos)
         #musica[cancion.strip()]=cantante.strip()
     
@@ -25,7 +27,12 @@ def agregarCanciones():
     for i in range(elementos):
         cancion=input("Ingrese el nombre de la cancion: ")
         cantante=input("Ingrese el nombre del cantante: ")
-        musica[cancion]=cantante
+        genero=input("Ingrese el genero de la cancion: ")
+        datos={}
+        datos["Nombre"]=cancion
+        datos["Cantante"]=cantante
+        datos["Genero"]=genero
+        musica.append(datos)
     
     print("================================")
     print("Cantante y cancion agregados")
@@ -34,10 +41,11 @@ def agregarCanciones():
 #Eliminamos una cancion del diccionario
 def eliminarCanciones():
     cancion=input("Ingresa nombre de la cancion que deseas eliminar:")
-    if cancion in musica:
-        del musica[cancion]
-        print("================================")
-        print("La cancion "+cancion+" ha sido eliminada")
+    for i in musica:
+        if i["Nombre"]==cancion:
+            musica.remove(i)
+            print("================================")
+            print("La cancion "+cancion+" ha sido eliminada")
     else:
         print("================================")
         print("La cancion no se encuentra en la lista")
@@ -58,13 +66,13 @@ def buscarCancionesporCantante():
         
 
 #Guardar lista de reproduccion en un archivo
-def guardarListaReproduccionArchivo(listaCancionesAleatorias):
+def guardarListaReproduccionArchivo():
     # Abre (o crea si no existe) el archivo en modo de escritura
     fichero = open("playlistAleatoria.txt", "w")
     
     # Escribe cada canción en una nueva línea del archivo
-    for cancion in listaCancionesAleatorias:
-        fichero.write(cancion + "\n")
+    for cancion in musica:
+        fichero.write(f"{cancion["Nombre"]} - {cancion["Cantante"]} - {cancion["Genero"]}\n")
     
     # Cierra el archivo después de escribir en él
     fichero.close()
@@ -79,11 +87,12 @@ agregarMusicaArchivo()
 #print("Canciones y cantantes")
 #print(musica)
 
-#agregarCanciones()
-#print(musica)
-#eliminarCanciones()
+agregarCanciones()
+print(musica)
+eliminarCanciones()
 #print(musica)
 #contarCanciones()
 #buscarCancionesporCantante()
 #crearListaReproduccionAleatoria()
 #guardarListaReproduccionArchivo(listaCancionesAleatorias)
+guardarListaReproduccionArchivo()
